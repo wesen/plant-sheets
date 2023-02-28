12c65b701a7d68549a1cec72b7793d677c0eab6f
Manuel Odendahl <wesen@ruinwesen.com>
2023-02-28 11:33:32 -0500

    :lipstick: :poop: Add CSS styling and external HTML template

--- src/code.js
+++ src/code.js
@@ -7,7 +7,7 @@ function onOpen() {
 
 function showSidebar() {
   var html = HtmlService.createHtmlOutput(`
-    <button onclick="google.script.run.showQuickFilterModal()">Create Quick Filter</button>
+    <button onclick="google.script.run.showQuickFilterDialog()">Create Quick Filter</button>
   `);
   html.setTitle('Products Helpers');
   html.setWidth(100);
@@ -15,25 +15,10 @@ function showSidebar() {
   SpreadsheetApp.getUi().showSidebar(html);
 }
 
-function showQuickFilterModal() {
-  var html = HtmlService.createHtmlOutput(`
-    <form>
-      <label for="filter-type">Create new quick filter:</label>
-      <select id="filter-type" name="filter-type">
-        <option value="name">Name</option>
-        <option value="sku">SKU</option>
-        <option value="category">Category</option>
-        <option value="species">Species</option>
-      </select>
-      <input type="text" id="filter-text" name="filter-text" placeholder="Enter filter text...">
-      <br>
-      <button type="button" onclick="google.script.run.createQuickFilter(document.getElementById('filter-type').value, document.getElementById('filter-text').value)">Create quick filter</button>
-      <button type="button" onclick="google.script.host.close()">Cancel</button>
-    </form>
-  `);
-  var title = 'Create Quick Filter';
+function showQuickFilterDialog() {
+  var template = HtmlService.createTemplateFromFile('quick-filter-dialog');
   var ui = SpreadsheetApp.getUi();
-  ui.showModalDialog(html, title);
+  var dialog = ui.showModalDialog(template.evaluate(), 'Create Quick Filter');
 }
 
 function createQuickFilter(column, text) {
